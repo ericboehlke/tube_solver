7dae96dcc4ec3c0271faad006172252fcf008bd6
Eric Boehlke <eric.p.boehlke@gmail.com>
2022-01-26 18:52:23 -0600

    Learn about .unwrap()

--- src/main.rs
+++ src/main.rs
@@ -11,15 +11,12 @@ struct Cli {
 
 fn main() {
     let args = Cli::parse();
-    let filepath = args.tubes_file_path.as_os_str().clone();
+    let filepath = args.tubes_file_path.as_os_str().to_str().unwrap();
     let contents = fs::read_to_string(args.tubes_file_path.clone())
         .expect("Something went wrong while reading the file");
     let tube_array: TubeArray = toml::from_str(&contents).unwrap();
     let initial_state = TubeState::from_tube_array(tube_array);
-    match filepath.to_str() {
-        Some(fp) => { println!("Reading tubes from {}.", fp); },
-        None => { println!("Could not read tube file path.") },
-    }
+    println!("Reading tubes from {}.", filepath);
     println!("\n-- Initial State ------------\n");
     println!("{}", initial_state);
     let solved_state = solve_bfs(&initial_state);
